07f69fef6a90312826eae0b5a271672824d06dda
mahdi-morovati <morovati155@gmail.com>
2025-04-27 21:49:27 +0330

    create ExceptionMiddleware.cs

---
+++ TodoApp.Api/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,75 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using TodoApp.Api.Middlewares.Models;
+using TodoApp.Application.Exceptions;
+
+namespace TodoApp.Api.Middlewares;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception e)
+        {
+            await HandleExceptionAsync(httpContext, e);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+    {
+        HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+        CustomProblemDetails problem = new();
+
+        switch (exception)
+        {
+            case BadRequestException badRequestException:
+                statusCode = HttpStatusCode.BadRequest;
+                problem = new CustomProblemDetails
+                {
+                    Title = badRequestException.Message,
+                    Status = (int)statusCode,
+                    Detail = badRequestException.InnerException?.Message,
+                    Type = nameof(BadRequestException),
+                    Errors = badRequestException.ValidationErrors
+                };
+                break;
+            case NotFoundException notFoundException:
+                statusCode = HttpStatusCode.NotFound;
+                problem = new CustomProblemDetails
+                {
+                    Title = notFoundException.Message,
+                    Status = (int)statusCode,
+                    Detail = notFoundException.InnerException?.Message,
+                    Type = nameof(NotFoundException),
+                };
+                break;
+            default:
+                problem = new CustomProblemDetails
+                {
+                    Title = exception.Message,
+                    Status = (int)statusCode,
+                    Detail = exception.StackTrace,
+                    Type = nameof(HttpStatusCode.InternalServerError),
+                };
+                break;
+        }
+
+        httpContext.Response.StatusCode = (int)statusCode;
+        var logMessage = JsonConvert.SerializeObject(problem);
+        _logger.LogError(logMessage);
+        await httpContext.Response.WriteAsJsonAsync(problem);
+    }
+}
--- TodoApp.Api/Program.cs
+++ TodoApp.Api/Program.cs
@@ -1,3 +1,4 @@
+using TodoApp.Api.Middlewares;
 using TodoApp.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +20,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 
 
