177a7f6189b7b247890fecbbf1b3dcc7f0048a3e
mahdi-morovati <morovati155@gmail.com>
2025-04-27 17:35:43 +0330

    create Task

---
+++ TodoApp.Domain/Entities/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp.Domain.Entities;
+
+public class Task
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime DueDate { get; set; }
+}
