71d2d64168caf4556a50a665a25526eb8d8a12ef
mahdi-morovati <morovati155@gmail.com>
2025-04-27 18:08:38 +0330

    create PersistenceServiceRegistration.cs

---
+++ TodoApp.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApp.Application.Contracts.Persistence;
+using TodoApp.Persistence.Contexts;
+using TodoApp.Persistence.Repositories;
+
+namespace TodoApp.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<TodoAppContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("TodoAppConnectionString"));
+        });
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+        return services;
+    }
+}
