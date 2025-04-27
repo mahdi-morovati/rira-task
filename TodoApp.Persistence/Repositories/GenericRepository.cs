bae570fdc608f9eb7ca39e10bd2a0d3cfb189de9
mahdi-morovati <morovati155@gmail.com>
2025-04-27 18:00:49 +0330

    create GenericRepository.cs

---
+++ TodoApp.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Application.Contracts.Persistence;
+using TodoApp.Domain.Entities;
+using TodoApp.Persistence.Contexts;
+
+namespace TodoApp.Persistence.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    protected readonly TodoAppContext Context;
+
+    public GenericRepository(TodoAppContext context)
+    {
+        Context = context;
+    }
+
+    public async Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        return await Context.Set<T>().AsNoTracking().ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(Guid id)
+    {
+        return await Context.Set<T>().AsNoTracking().FirstOrDefaultAsync(q => q.Id == id);
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await Context.AddAsync(entity);
+        await Context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        Context.Entry(entity).State = EntityState.Modified;
+        await Context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        Context.Remove(entity);
+        await Context.SaveChangesAsync();
+    }
+}
